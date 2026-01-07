35bb3fc0439ac0a677c5883d3eb9e44a06160f9f
SamanPrasad <samanseedr2@gmail.com>
2026-01-07 16:31:47 +0530

    port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const PORT = process.env.POST || 8080;
+const PORT = process.env.SERVER_PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
