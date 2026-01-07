8f499bb46109d646cd8d2e62ab7cedd429c4cd8b
SamanPrasad <samanseedr2@gmail.com>
2026-01-07 23:30:01 +0530

    origin

--- src/index.ts
+++ src/index.ts
@@ -6,18 +6,18 @@ dotenv.config();
 const app = express();
 
 const PORT = Number(process.env.SERVER_PORT) || 8080;
-const ORIGIN = process.env.ORIGIN;
-if (!ORIGIN) throw new Error("No origin found!");
+// const ORIGIN = process.env.ORIGIN;
+// if (!ORIGIN) throw new Error("No origin found!");
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.use(
   cors({
-    origin: ORIGIN,
+    origin: "https://celestee.co.uk",
     allowedHeaders: "*",
     methods: "*",
-    credentials: true,
+    credentials: false,
   })
 );
 
